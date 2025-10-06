4d633f64efb492909de4fb76cc46467caefb7dc5
Marcos Camargo <mcmpgui12@gmail.com>
2025-10-06 16:52:29 -0300

    Subindo os exercicios

--- tipos_dados.py
+++ tipos_dados.py
@@ -5,3 +5,15 @@ print(type(var))
 
 # print é saida
 #input é entrada
+
+# constante é imutavael
+# variável é mutável
+
+# em python não existe constante, apenas variável
+# porem podemos usar convencao para indicar que uma variavel é constante
+# exemplo: PI = 3.14
+
+# Boas praticas para nomes 
+# Usar o padrao snake case
+# Escolher nomes sugestivos
+# Nome de constantes em maiusculo
---
+++ variaveis_const.py
@@ -0,0 +1,12 @@
+nome = "Marcos"
+IDADE = 21
+
+
+nome = "Joao"
+print(nome, IDADE)
+
+
+limite_saque_diario = 1000
+
+STATES =  ["SP", "RJ", "MG"]
+
