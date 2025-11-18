bd429a6783944418e0a69d1b7ce345cdf42401e0
Marcos Camargo <mcmpgui12@gmail.com>
2025-11-18 01:08:49 -0300

    Feat: Adicionando Operadores

---
+++ TiposDeOperadores/OperadoresAri.py
@@ -0,0 +1,23 @@
+# x = 10 - 5 * 2
+
+# print(x)
+    
+
+
+
+# Em python e demais linguagens a ordem de precedência dos operadores é:
+# 1. Parênteses
+# 2. Exponenciação
+# 3. Multiplicação e Divisão
+# 4. Adição e Subtração
+
+produto1 = 10
+produto2 = 20
+
+print(produto1 + produto2 ) 
+print(produto1 * produto2 )
+print(produto2 ** produto1)
+print(produto2 / produto1 )
+print(produto2 - produto1 )
+print(produto2 // produto1 )
+print(produto2 % produto1 )
---
+++ TiposDeOperadores/OperadoresComparacoes.py
@@ -0,0 +1,12 @@
+saldo = 450
+saque = 200
+
+print(saldo == saque)  # false --> == Igual
+
+print(saldo != saque)  # True --> ! = Diferente
+
+print(saldo > saque)  # True --> > Maior
+print(saldo < saque)  # False --> < Menor
+
+print(saldo >= saque)  # True --> >= Maior ou igual
+print(saldo <= saque)  # False --> <= Menor ou igual
