c6f40787988a2e00c3b1dac5f4a7a1ebb59ace75
Marcos Camargo <mcmpgui12@gmail.com>
2025-11-18 01:18:55 -0300

    Feat: Adicionando Operadores Logicos etc...

---
+++ TiposDeOperadores/OperadoresDeAtribuicao.py
@@ -0,0 +1,23 @@
+saldo = 500 # Atribuição simples
+print(saldo)
+
+saldo += 200 # Atribuição com adição 
+print(saldo)
+
+saldo -= 100 # Atribuição com subtração
+print(saldo)
+
+saldo *= 2 # Atribuição com multiplicação
+print(saldo)
+
+saldo /= 4 # Atribuição com divisão
+print(saldo)
+
+saldo %= 3 # Atribuição com módulo
+print(saldo)
+
+saldo //= 1 # Atribuição com divisão inteira
+print(saldo)
+
+saldo **= 4 # Atribuição com exponenciação
+print (saldo)
