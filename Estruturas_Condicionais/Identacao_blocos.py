59b1ef5a5ccb3dc15e932c308d81fbf19bea8dcd
Marcos Camargo <mcmpgui12@gmail.com>
2025-11-18 01:56:29 -0300

    Feat: Adicionando Identacao em blocos

---
+++ Estruturas_Condicionais/Identacao_blocos.py
@@ -0,0 +1,17 @@
+def sacar(valor: float):
+    saldo = 500
+    
+    if saldo >= valor:
+        print("VALOR SACADO")
+        print("Retire seu dinheiro no caixa")
+    
+    print("Obrigado por usar nosso caixa eletrônico")
+    
+def depositar(valor: 500):
+    saldo = 500
+    
+    saldo += valor
+    print("VALOR DEPOSITADO")
+
+
+sacar(100)
