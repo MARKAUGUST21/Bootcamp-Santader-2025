d067655a41d1c42f776974b55138b41bfa815343
Marcos Camargo <mcmpgui12@gmail.com>
2025-11-20 18:19:43 -0300

    feat: Fatiamento Strings

---
+++ Strings_Fatiamentos/FatiamentoStr.py
@@ -0,0 +1,10 @@
+nome = "Marcos Pedro Camargo Augusto Almeida"
+
+print(nome[0])          # Primeiro caractere
+print(nome[1])          # Segundo caractere
+print(nome[2])          # Terceiro caractere
+print(nome[3])          # Quarto caractere
+
+print(nome[0:15])     # Fatiamento do índice 0 ao 14
+print(nome[16:])      # Fatiamento do índice 16 até o final
+print(nome[:15])      # Fatiamento do início até o índice 14
