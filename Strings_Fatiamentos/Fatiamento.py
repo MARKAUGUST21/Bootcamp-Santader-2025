11aee781572f3a80f1da7cbffd81fd9969ef3935
Marcos Camargo <mcmpgui12@gmail.com>
2025-11-20 17:29:34 -0300

    Feat: Fatiamento de Strings

---
+++ Strings_Fatiamentos/Fatiamento.py
@@ -0,0 +1,10 @@
+# Oque iremos ver nesse arquivo:
+# Conhecendo metodos uteis da classe str
+# Interpolação de variaveis 
+# Fatiamento de strings
+# Strings de multiplas linhas
+
+
+#.upper = Transforma todos os caracteres em maiusculo
+#.lower = Transforma todos os caracteres em minusculo
+#.title = Transforma o primeiro caractere de cada palavra em maiusculo 
