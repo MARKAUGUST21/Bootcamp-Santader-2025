592b3d57180b831f4d6d671ae6468f0aef74a389
Marcos Camargo <mcmpgui12@gmail.com>
2025-11-20 17:51:16 -0300

    Feat: Subindo Marcacoes e Interpolacao de strings

--- Strings_Fatiamentos/Fatiamento.py
+++ Strings_Fatiamentos/Fatiamento.py
@@ -1,10 +1,17 @@
-# Oque iremos ver nesse arquivo:
-# Conhecendo metodos uteis da classe str
-# Interpolação de variaveis 
-# Fatiamento de strings
-# Strings de multiplas linhas
+nome = "gUIlherme"
+
+print(nome.upper()) # Transforma todos os caracteres em maiusculo
+print(nome.lower()) # Transforma todos os caracteres em minusculo
+print(nome.title()) # Transforma o primeiro caractere de cada palavra em maiusculo
+
+texto = "    Ola mundo!"
+
+print(texto.strip() + ".")  # Elimina os espaços em branco no inicio e no fim da string
+print(texto.lstrip() + ".") # Elimina os espaços em branco no inicio da string
+print(texto.rstrip() + ".") # Elimina os espaços em branco no fim da string
 
 
-#.upper = Transforma todos os caracteres em maiusculo
-#.lower = Transforma todos os caracteres em minusculo
-#.title = Transforma o primeiro caractere de cada palavra em maiusculo 
+menu = "Python"
+
+print("###" + menu + "###")# Centraliza a string de acordo com o numero de caracteres informado
+print(menu.center (12, "#")) # Centraliza a string de acordo com o numero de caracteres informado
---
+++ Strings_Fatiamentos/README.md
@@ -0,0 +1,24 @@
+# Oque iremos ver nesse arquivo:
+# Conhecendo metodos uteis da classe str
+# Interpolação de variaveis 
+# Fatiamento de strings
+# Strings de multiplas linhas
+
+
+    ##.upper = Transforma todos os caracteres em maiusculo
+    ##.lower = Transforma todos os caracteres em minusculo
+    ##.title = Transforma o primeiro caractere de cada palavra em maiusculo 
+
+# Eliminando espaços em branco no inicio e no fim de uma string
+    ###.strip() = Elimina os espaços em branco no inicio e no fim da string
+    ###.lstrip() = Elimina os espaços em branco no inicio da string
+    ###.rstrip() = Elimina os espaços em branco no fim da string
+
+# Centralizando e juncoes de strings
+    ###.center() = Centraliza a string de acordo com o numero de caracteres informado
+    ###.join() = Junta uma lista de strings em uma unica string, utilizando o caractere informado como separador
+
+# Interpolacao de Strings 
+    ### Em python temos 3 formas de interpolar as strings, primeira que temos é o sinal %, a segunda é metodo format, e a ultima é f strings.
+
+    
