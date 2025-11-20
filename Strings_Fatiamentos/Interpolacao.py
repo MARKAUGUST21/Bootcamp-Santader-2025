beda81d9c7f86c6a0da0d6fa40ff9ec4effbb6a0
Marcos Camargo <mcmpgui12@gmail.com>
2025-11-20 18:12:48 -0300

    feat:

---
+++ Strings_Fatiamentos/Interpolacao.py
@@ -0,0 +1,8 @@
+nome = "Marcos"
+idade = 23
+profissao = "Desenvolvedor"
+linguagem = "Python"
+
+print(f"Ola me chamo {nome}, tenho {idade} anos, sou {profissao} e uso a linguagem {linguagem} para desenvolver meus projetos.")
+print("Nome %s  Idade %d" % (nome, idade))
+print("Nome: {} Idade: {} Profissao: {} Linguagem: {}".format(nome, idade, profissao, linguagem))
