65be7281e43e6ccc91a364a7daf17c98c337bbcb
Marcos Camargo <mcmpgui12@gmail.com>
2025-11-22 03:44:17 -0300

    feat: funcoes

---
+++ Funcoes/funcoes.py
@@ -0,0 +1,22 @@
+def exibir_mensagem():
+    print("Esta é uma função de exemplo.")
+    
+
+
+def exibir_mensagem2(nome):
+    print(f"Olá, {nome}!")
+    
+    
+def exibir_mensagem3(nome = "Usuário"):
+    print(f"Olá, {nome}!")
+    
+
+
+exibir_mensagem()
+exibir_mensagem2("Guilherme")
+exibir_mensagem3()
+
+
+
+
+
